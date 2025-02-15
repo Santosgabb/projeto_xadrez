c42439e0a53621589603ae3549b56ca5ab2b2d31
Gabriel Ryan <ryangabriel.gr77@gmail.com>
2025-02-14 21:07:56 -0300

    Handling pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -12,11 +15,14 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 	
+	private List<Piece> piecesOnYheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	//tamanhp do tabuleiro
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
 		currentPlayer = Color.WHITE;
+	
 		initialSetup();
 	}
 	
@@ -74,6 +80,10 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		if (capturedPiece != null) {
+			piecesOnYheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 
@@ -83,6 +93,7 @@ public class ChessMatch {
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece ) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnYheBoard.add(piece);
 	}
 	
 	private void initialSetup() {
